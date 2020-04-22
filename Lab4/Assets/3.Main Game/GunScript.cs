b19277bbd8b898640c6e3109c1bc8d9c93ae566f
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-04-22 15:39:36 -0600

    Gun script

--- Lab4/Assets/3.Main Game/GunScript.cs
+++ Lab4/Assets/3.Main Game/GunScript.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private LineRenderer laserLine;
+    private float nextFire;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        laserLine = GetComponent<LineRenderer>();
+        fpsCam = GetComponentInParent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown ("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
     }
 }
