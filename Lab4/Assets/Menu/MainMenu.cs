e733568103eb0e695f8ae1192c6fb4d35eafb49e
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-04-21 21:55:17 -0600

    Second Game

--- Lab4/Assets/Menu/MainMenu.cs
+++ Lab4/Assets/Menu/MainMenu.cs
@@ -5,21 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+    }
+
+    public void StartSecondGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
         Time.timeScale = 1f;
     }
 
